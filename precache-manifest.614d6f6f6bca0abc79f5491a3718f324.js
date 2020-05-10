18c6b6fdefd288875cf8503b226329cb79606860
Ritesh Dhawad <ritesh@simplecrm.com.sg>
2020-05-10 15:02:32 +0530

    Updates

---
+++ precache-manifest.614d6f6f6bca0abc79f5491a3718f324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2a9267d6b53c2bf1fdd965b28f77f4",
+    "url": "/reactgit/index.html"
+  },
+  {
+    "revision": "06f4ca937c2821675f4f",
+    "url": "/reactgit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "122c4dda91092c3f0127",
+    "url": "/reactgit/static/js/2.a467cb91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactgit/static/js/2.a467cb91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f4ca937c2821675f4f",
+    "url": "/reactgit/static/js/main.0f0890bd.chunk.js"
+  },
+  {
+    "revision": "0e2db082af4fa6602461",
+    "url": "/reactgit/static/js/runtime-main.0f8eb72b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactgit/static/media/logo.5d5d9eef.svg"
+  }
+]);
